0478937ae0295d5a377ca220251b08614bf96762
Will Gester <wiilgester@gmail.com>
2013-11-25 12:26:46 -0800

    initial recommit with some already finished specs - starting git workflow

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -10,34 +10,29 @@ describe("About Higher Order Functions", function () {
   it("should use filter to return array items that meet a criteria", function () {
     var numbers = [1,2,3];
     var odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
-    
+
     expect(odd).toEqual(FILL_ME_IN);
     expect(odd.length).toBe(FILL_ME_IN);
     expect(numbers.length).toBe(FILL_ME_IN);
   });
-    
+
   it("should use 'map' to transform each element", function () {
     var numbers = [1, 2, 3];
     var numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
-    
+
     expect(numbersPlus1).toEqual(FILL_ME_IN);
     expect(numbers).toEqual(FILL_ME_IN);
   });
-    
+
   it("should use 'reduce' to update the same result on each iteration", function () {
     var numbers = [1, 2, 3];
     var reduction = _(numbers).reduce(
-      function(memo, x) {
-        //note: memo is the result from last call, and x is the current number
-        return memo + x;
-      }, 
-      /* initial */ 0
-    );
-    
+            function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
+
     expect(reduction).toBe(FILL_ME_IN);
     expect(numbers).toEqual(FILL_ME_IN);
   });
-    
+
   it("should use 'forEach' for simple iteration", function () {
     var numbers = [1,2,3];
     var msg = "";
@@ -46,11 +41,11 @@ describe("About Higher Order Functions", function () {
     };
 
     _(numbers).forEach(isEven);
-    
+
     expect(msg).toEqual(FILL_ME_IN);
     expect(numbers).toEqual(FILL_ME_IN);
   });
-    
+
   it("should use 'all' to test whether all items pass condition", function () {
     var onlyEven = [2,4,6];
     var mixedBag = [2,4,5,6];
@@ -60,7 +55,7 @@ describe("About Higher Order Functions", function () {
     expect(_(onlyEven).all(isEven)).toBe(FILL_ME_IN);
     expect(_(mixedBag).all(isEven)).toBe(FILL_ME_IN);
   });
-    
+
   it("should use 'any' to test if any items passes condition" , function () {
     var onlyEven = [2,4,6];
     var mixedBag = [2,4,5,6];
@@ -72,23 +67,23 @@ describe("About Higher Order Functions", function () {
   });
 
   it("should use range to generate an array", function() {
-    expect(_.range(3)).toEqual(FILL_ME_IN);
-    expect(_.range(1, 4)).toEqual(FILL_ME_IN);
-    expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
+      expect(_.range(3)).toEqual(FILL_ME_IN);
+      expect(_.range(1, 4)).toEqual(FILL_ME_IN);
+      expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
   });
 
   it("should use flatten to make nested arrays easy to work with", function() {
-    expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
+      expect(_([ [1, 2], [3, 4] ]).flatten()).toEqual(FILL_ME_IN);
   });
 
   it("should use chain() ... .value() to use multiple higher order functions", function() {
-    var result = _([ [0, 1], 2 ]).chain()
-                     .flatten()
-                     .map(function(x) { return x+1 } )
-                     .reduce(function (sum, x) { return sum + x })
-                     .value();
+      var result = _([ [0, 1], 2 ]).chain()
+                       .flatten()
+                       .map(function(x) { return x+1 } )
+                       .reduce(function (sum, x) { return sum + x })
+                       .value();
 
-    expect(result).toEqual(FILL_ME_IN);
+      expect(result).toEqual(FILL_ME_IN);
   });
 
 });
